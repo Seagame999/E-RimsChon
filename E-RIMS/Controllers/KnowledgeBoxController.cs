3b0152830704e8b19df3bb5428dfbeb0f2696bb5
Sittinon SUPAPBURUS <sittinon.s@live.ku.th>
2020-04-12 19:50:16 +0700

    -เพิ่้มกล่องความรู้ -เพิ่มตัวติดตามงาน

---
+++ E-RIMS/Controllers/KnowledgeBoxController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_RIMS.Controllers
+{
+    public class KnowledgeBoxController : Controller
+    {
+        // GET: KnowledgeBox
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
