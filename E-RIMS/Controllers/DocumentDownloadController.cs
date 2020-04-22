6d2ecdbcc6df417f5317cb5c0df99339768f0364
Boonchoo JITNUPONG <boonchoo.ji@outlook.com>
2020-04-22 23:16:16 +0700

    update some views

---
+++ E-RIMS/Controllers/DocumentDownloadController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_RIMS.Controllers
+{
+    public class DocumentDownloadController : Controller
+    {
+        // GET: DocumentDownload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DocumentDownload
+        public ActionResult CreateDownloadDocument()
+        {
+            return View();
+        }
+    }
+}
---
+++ E-RIMS/Controllers/FundingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_RIMS.Controllers
+{
+    public class FundingController : Controller
+    {
+        // GET: Funding
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Funding()
+        {
+            return View();
+        }
+    }
+}
