9d9af01cd7a7f745d95557bcd39361e3795a7b41
Boonchoo JITNUPONG <boonchoo.ji@outlook.com>
2020-05-24 13:06:01 +0700

    commit js

--- E-RIMS/Scripts/updateactivity.js
+++ E-RIMS/Scripts/updateactivity.js
@@ -1,45 +1,45 @@
 ﻿
 
-    $(document).ready(function () {
-        var activityStatus = $.urlParam('status');
-        var res = decodeURI(activityStatus);
+$(document).ready(function () {
+    var activityStatus = $.urlParam('status');
+    var res = decodeURI(activityStatus);
 
-        var planningCard = $("#planningCard");
-        var workingCard = $("#workingCard");
-        var finishingCard = $("#finishingCard");
+    var planningCard = $("#planningCard");
+    var workingCard = $("#workingCard");
+    var finishingCard = $("#finishingCard");
 
-        var planningCheck = $("#planningCheck");
-        var workingCheck = $("#workingCheck");
-        var finishingCheck = $("#finishingCheck");
+    var planningCheck = $("#planningCheck");
+    var workingCheck = $("#workingCheck");
+    var finishingCheck = $("#finishingCheck");
 
-        if (activityStatus != null) {
+    if (activityStatus != null) {
 
-            if (res == 'ยังไม่ดำเนินการ') {
-                workingCard.hide();
-                finishingCard.hide();
+        if (res == 'ยังไม่ดำเนินการ') {
+            workingCard.hide();
+            finishingCard.hide();
 
-                planningCheck.addClass('inactive');
-                workingCheck.addClass('inactive');
-                finishingCheck.addClass('inactive');
-            } else if (res == 'วางแผน') {
-                planningCheck.addClass('active');
-                workingCheck.addClass('inactive');
-                finishingCheck.addClass('inactive');
+            planningCheck.addClass('inactive');
+            workingCheck.addClass('inactive');
+            finishingCheck.addClass('inactive');
+        } else if (res == 'วางแผน') {
+            planningCheck.addClass('active');
+            workingCheck.addClass('inactive');
+            finishingCheck.addClass('inactive');
 
-                finishingCard.hide();
-            } else if (res == 'ดำเนินการ') {
-                
-            }
-        }
-    });
+            finishingCard.hide();
+        } else if (res == 'ดำเนินการ') {
 
-    //get parameters function
-    $.urlParam = function (name) {
-        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-        if (results == null) {
-            return null;
-        }
-        else {
-            return results[1] || 0;
         }
     }
+});
+
+//get parameters function
+$.urlParam = function (name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results == null) {
+        return null;
+    }
+    else {
+        return results[1] || 0;
+    }
+}
